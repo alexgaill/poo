eb3c5a9376825bc4a81b96a8baeeb4cd6d1a6dc7
alexandre gaillot <alexandre@MacBook-Air-de-alexandre.local>
2020-04-20 17:25:18 +0200

    first commit création méthod createTable

---
+++ Config/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+class Config
+{
+
+    public function __construct(){
+        $this->config = [
+            'dbHost' => '127.0.0.1',
+            'dbPort' => '8889',
+            'dbName' => 'immobilier',
+            'dbUser' =>  'root',
+            'dbPassword' => 'root'
+        ];
+    }
+
+    public function getConfig(){
+        return $this->config;
+    }
+    
+}
---
+++ Database/CreateDatabase.php
@@ -0,0 +1,58 @@
+<?php
+
+require 'Database.php';
+
+class createDatabase extends Database{
+
+
+    /**
+     * Créé la table correspondante dans la Db
+     *
+     * @param string $table
+     * @param array $champs
+     * @param array $foreigns
+     */
+    public function createTable($table, $champs = [], $foreigns = []){
+
+        $statement = 'CREATE TABLE IF NOT EXISTS '. $table . '(';
+        
+        foreach ($champs as $key => $value) {
+            $statement .= $key . ' ' . $value . ',';
+        }
+        $statement = substr($statement, 0, -1);
+        if (!empty($foreigns)) {
+            $statement .= $this->makeForeign($foreigns);
+        }
+        $statement .= ")";
+        var_dump($statement);
+        $this->pdo->exec($statement);
+    }
+
+    /**
+     * Création de la table property
+     */
+    public function createTableProperty(){
+
+        $this->pdo->exec('CREATE TABLE IF NOT EXISTS property(
+            
+        )');
+    }
+
+    public function createTableOptionsProperty(){
+        $this->pdo->exec('CREATE TABLE optionsproperty(
+            
+            
+            )');
+    }
+
+    public function makeForeign($foreigns)
+    {
+        $statement = ", ";
+
+        foreach ($foreigns as $key => $value) {
+            $statement .= 'FOREIGN KEY(' . $key . ') REFERENCES ' . $value . ',';
+        }
+        $statement = substr($statement, 0, -1);
+        return $statement;
+    }
+}
